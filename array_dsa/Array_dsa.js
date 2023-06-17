1f82a7be003e275ccc428a1043014618f1c66c4d
Niradity Emmanuel <yadavrjking66997@gmail.com>
2023-06-17 07:24:09 +0530

    Searching key value in array

--- array_dsa/Array_dsa.js
+++ array_dsa/Array_dsa.js
@@ -1746,5 +1746,107 @@
 // }
 
 
+/** $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ */
+
+//Search an elemnent in a sorted and rotated Array
+//Binary Search 
+
+// function binarySearch(arr, low, high, key){
+//     if(high < low){
+//         return -1;
+//     }
+
+//     let mid = Math.floor(low + high /2);
+
+//     if(key == arr[mid]){
+//         return mid;
+//     }
+//     if(key > arr[mid]){
+//         return binarySearch(arr, (mid+1), high, key);
+//     }
+// }
+
+// function findPivot(arr, low, high){
+//     if(high < low){
+//         return -1;
+//     }
+//     if(high == low){
+//         return low;
+//     }
+
+//     let mid = Math.floor((low + high) / 2);
+//     if(mid < high && arr[mid] > arr[mid + 1]){
+//         return mid;
+//     }
+
+//     if(mid > low && arr[mid] < arr[mid - 1]){
+//         return (mid- 1);
+//     }
+
+//     if(arr[low] >= arr[mid]){
+//         return findPivot(arr, low, mid - 1)
+//     }
+//     return findPivot(arr, mid + 1, high);
+// }
+
+// function pivotedBinarySearch(arr, n, key){
+//     let pivot = findPivot(arr,0, n - 1);
+
+//     if(pivot == -1){
+//         return binarySearch(arr, 0, n - 1, key);
+//     }
+
+//     if(arr[pivot] == key){
+//         return pivot;
+//     }
+//     if(arr[0] <= key){
+//         return binarySearch(arr, pivot + 1, n - 1, key);
+//     }
+//     return binarySearch(arr, pivot + 1, n - 1, key);
+// }
+
+// //Driver code
+
+// let arr1 = [5, 6, 7, 8,9, 10, 1, 2, 3];
+// let n = arr1.length; 
+// let key = 3;
+// console.log("Index of the element is" + " " + pivotedBinarySearch(arr1, n , key));
 
 
+/** %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+
+// function searchElement(arr, l, h, key){
+//     if( l > h){
+//         return -1;
+//     }
+
+//     let mid = Math.floor((l + h) / 2);
+//     if(arr[mid] == key){
+//         return mid;
+//     }
+
+//     /** If arr[l....mid] is sorted */
+//     if(arr[l] <= arr[mid]){
+//         if(key >= arr[l] && key <= arr[mid]){
+//             return searchElement(arr, l, mid-1, key);
+//         }
+//         return searchElement(arr, mid + 1, h, key);
+
+//     }
+
+//     if(key >= arr[mid] && key <= arr[h]){
+//         return searchElement(arr, mid + 1, h, key);
+//     }
+//     return searchElement(arr, l, mid - 1, key);
+// }
+
+// //Driver code
+// let arr = [4, 5, 6, 7, 8,9, 1, 2, 3];
+// let n=arr.length;
+// let key = 3; 
+// let i = searchElement(arr, 0, n - 1, key);
+// if( i != -1){
+//     console.log("Index" + i+ "\n");
+// }else{
+//     console.log("Key not found");
+// }
